f3861b52eec9a33844e664fb1b3d611f46848410
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-08-17 13:42:54 -0400

    Update __init__.py

--- andes/models/synchronous/__init__.py
+++ andes/models/synchronous/__init__.py
@@ -4,4 +4,5 @@ Package for synchronous generators
 
 from andes.models.synchronous.gencls import GENCLS  # NOQA
 from andes.models.synchronous.genrou import GENROU  # NOQA
+from andes.models.synchronous.genrouf1 import GENROUF1
 from andes.models.synchronous.plbvfu1 import PLBVFU1  # NOQA
