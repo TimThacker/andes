5c6d17a2fff461faa0480fbef76acef7cd60d772
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-08-20 16:40:55 -0400

    Update pert.py

--- andes/cases/smib/pert.py
+++ andes/cases/smib/pert.py
@@ -29,27 +29,6 @@ def pert(t, system):
     system : andes.system.System
         System object supplied by the simulator.
     """
-    coordination = [0.5999,
-                    0.5332,
-                    0.5084,
-                    0.4998,
-                    0.462541521,
-                    0.429121226,
-                    0.340299815,
-                    0.282600701,
-                    0.276334584,
-                    0.289933205,
-                    0.269690573,
-                    0.287803233,
-                    0.295975417,
-                    0.302936882,
-                    0.278517008,
-                    0.290291786,
-                    0.228976682,
-                    0.274061799,
-                    0.288022757,
-                    0.285998553,
-                    0.315802068]
 
 
     if t > 0:
@@ -57,45 +36,45 @@ def pert(t, system):
 
         
     if t == 0:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.5999)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.005999)
     if t == 0.1:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.5332)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.005332)
     if t == 0.2:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.5084)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.005084)
     if t == 0.3:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.4998)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.004998)       
     if t == 0.4:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.462541521)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00462541521)
     if t == 0.5:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.429121226)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00429121226)       
     if t == 0.6:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.340299815)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00340299815)        
     if t == 0.7:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.282600701)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00282600701)        
     if t == 0.8:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.276334584)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00276334584)        
     if t == 0.9:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.289933205)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00289933205)       
     if t == 1.0:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.269690573)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00269690573)       
     if t == 1.1:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.287803233)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00287803233)
     if t == 1.2:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.295975417)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00295975417)
     if t == 1.3:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.302936882)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00302936882)
     if t == 1.4:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.278517008)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00278517008)       
     if t == 1.5:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.290291786)
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00290291786)
     if t == 1.6:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.228976682)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00228976682)       
     if t == 1.7:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.274061799)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00274061799)        
     if t == 1.8:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.288022757)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00288022757)        
     if t == 1.9:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.285998553)        
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00285998553)        
     if t == 2.0:
-        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.315802068)       
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', 0.00315802068)       
    
