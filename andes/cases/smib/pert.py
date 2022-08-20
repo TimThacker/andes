400e8ea0a6ce9167bf8c2555b082f663455970e5
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-08-20 16:29:46 -0400

    Update pert.py

--- andes/cases/smib/pert.py
+++ andes/cases/smib/pert.py
@@ -29,6 +29,32 @@ def pert(t, system):
     system : andes.system.System
         System object supplied by the simulator.
     """
+    coordination = [0.5999,
+                    0.5332,
+                    0.5084,
+                    0.4998,
+                    0.462541521,
+                    0.429121226,
+                    0.340299815,
+                    0.282600701,
+                    0.276334584,
+                    0.289933205,
+                    0.269690573,
+                    0.287803233,
+                    0.295975417,
+                    0.302936882,
+                    0.278517008,
+                    0.290291786,
+                    0.228976682,
+                    0.274061799,
+                    0.288022757,
+                    0.285998553,
+                    0.315802068]
+
 
     if t > 0:
         system.PQ.alter('Req', 'PQ_1', 1.05)
+        
+    if t <= 2:
+        x=t*10
+        system.IEESGORM.alter('uomega0', 'IEESGORM_1', coordination(x)
