4fac827712e51a0e6d1d21acb45fc971ed4d2fa8
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-04-26 18:18:21 -0400

    Update solverbase.py

--- andes/linsolvers/solverbase.py
+++ andes/linsolvers/solverbase.py
@@ -1,13 +1,6 @@
 from andes.linsolvers.cupy import CuPySolver
 from andes.linsolvers.scipy import SpSolve
 from andes.linsolvers.suitesparse import UMFPACKSolver, KLUSolver
-from numpy import angle, conj, exp, r_, Inf
-from numpy.linalg import norm
-from scipy.sparse.linalg import splu
-from scipy import linalg
-from qiskit import QuantumCircuit
-from qiskit.algorithms.linear_solvers.hhl import HHL
-from qiskit import quantum_info
 
 class Solver:
     """
@@ -71,26 +64,6 @@ class Solver:
         """
         return self.worker.linsolve(A, b)
     
-    def quantum_solve(self, A, b):
-        """
-        Solve linear equations using the HHL algorithm
-        ------
-        This requires the A matrix to be Hermitian.
-        """
-        isHermitian = scipy.linalg.ishermitian(A)
-        if isHermitian is False:
-            A = A.getH()
-        
-        num_qubits = int(np.log2(A.shape[0]))
-        b_norm = b / np.linalg.norm(b)
-        qc = QuantumCircuit(num_qubits)
-        qc.isometry(b_norm, list(range(num_qubits)), None)
-        hhl = HHL()
-        solution = hhl.solve(A, b_norm)
-        total_qubits = solution.state.num_qubits
-        approx_result = quantum_info.Statevector(solution.state).data[2 ** (total_qubits - 1) : 2 ** (total_qubits - 1) + 2 ** num_qubits]
-        mismatch = abs(approx_result)*np.lingalg.norm(b) # Multiple the approximate result by the norm
-        return mismatch
 
     def clear(self):
         """
